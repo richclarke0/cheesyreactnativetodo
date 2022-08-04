08af5ba1dbe9a621752c861a2a576451e1ec6fd5
Richard Clarke <freshnewrich@gmail.com>
2022-08-04 14:49:50 -0700

    TaskItem, app.js mod

--- App.js
+++ App.js
@@ -1,12 +1,36 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Keyboard, ScrollView, StyleSheet, Text, View } from 'react-native';
+import TaskItem from './TaskItem';
 
 export default function App() {
+  const [tasks, setTasks] = useState()
+
+  const addTask = (task) => {
+    if (task == null) return;
+    setTasks([...tasks,task]);
+    Keyboard.dismiss()
+  }
+
+  const deleteTask = (deleteIndex) => {
+    setTasks(tasks.filter((value,index) => index != deleteIndex))
+  }
   return (
     <View style={styles.container}>
       {/* <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" /> */}
       <Text style={styles.heading}>TODO LIST</Text>
+      <ScrollView style={styles.scrollView}>
+        {
+          tasks.map((task, index) => {
+            return (
+              <View key={index} style={styles.taskContainer}>
+                <TaskItem index={index+1} task={task} deleteTask={() => deleteTask(index)}/>
+              </View>
+            )
+          })
+        }
+      </ScrollView>
+      <TaskInputField addTask={addTask}/>
     </View>
   );
 }
@@ -26,4 +50,10 @@ const styles = StyleSheet.create({
     marginBottom: 10,
     marginLeft: 20,
   },
+  scrollView: {
+    marginBottom: 70,
+  },
+  taskContainer: {
+    marginTop: 20,
+  }
 });
